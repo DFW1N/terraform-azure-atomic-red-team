c1814bb1647ecec53ad460f8a9e1dfc6e8ad544e
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-11-07 21:37:51 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+##############################################
+# Author: Sacha Roussakis-Notter (DFW1N)     #
+#                                           
+# Th
 
+resource "azurerm_resource_group" "rg-atomic-red-team" {
+  name     = "rg-atomic-red-team"
+  location = "West Europe"
+}
