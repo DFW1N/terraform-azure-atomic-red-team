cc83a3382e158b56cd3199c672e5b7c285814655
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-11-07 22:10:20 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,6 +58,7 @@ resource "azurerm_virtual_network" "atomic-vnet" {
     modules           = "atomic-red-team"
     author            = "Sacha Roussakis-Notter (DFW1N)"
     repo              = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment        = "red-teaming-activity"
   }
 }
 
@@ -69,4 +70,23 @@ resource "azurerm_network_security_group" "atomic-nsg" {
   name                = "AtomicRedTeamNetworkSecurityGroup"
   location            = azurerm_resource_group.rg-atomic-red-team.location
   resource_group_name = azurerm_resource_group.rg-atomic-red-team.name
+  
+  security_rule {
+    name                       = "AtomicAllowAll"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    modules           = "Atomic-Red-Team"
+    author            = "Sacha Roussakis-Notter (DFW1N)"
+    repo              = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment        = "red-teaming-activity"
+  }
 }
