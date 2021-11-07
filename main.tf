e420e8b8ca9ff39da22b28b63e78ff2f2141462b
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-11-07 22:26:46 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@
 #                            \ \__\\ _\\ \_______\ \_______\           \ \__\ \ \_______\ \__\ \__\ \__\    \ \__\                     #
 #                             \|__|\|__|\|_______|\|_______|            \|__|  \|_______|\|__|\|__|\|__|     \|__|                     #                                                                                                                    
 #                                                                                                                                      #
-#                                             Project:          CRIMOL-SR Transformation                                               #
+#                                             Project:          Azure Terraform Atomic Red Team                                        #
 #                                             Creator:          Sacha Roussakis-Notter (DFW1N)                                         #
 #                                             Creation Date:    Sunday, November 7th 2021, 10:16 pm                                    #
 #                                                                                                                                      #
@@ -26,13 +26,14 @@
 ####################
 
 resource "azurerm_resource_group" "rg-atomic-red-team" {
-  name                = "rg-atomic-red-team"
-  location            = "West Europe"
+  name                         = "rg-atomic-red-teaming"
+  location                     = "West Europe"
+  
   tags = {
-    modules           = "atomic-red-team"
-    author            = "Sacha Roussakis-Notter (DFW1N)"
-    repo              = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
-    assessment        = "red-teaming-activity"
+    modules                    = "atomic-red-team"
+    author                     = "Sacha Roussakis-Notter (DFW1N)"
+    repo                       = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment                 = "red-teaming-activity"
   }
 }
 
@@ -41,24 +42,24 @@ resource "azurerm_resource_group" "rg-atomic-red-team" {
 #####################
 
 resource "azurerm_virtual_network" "atomic-vnet" {
-  depends_on          = [azurerm_network_security_group.atomic-nsg]
-  name                = "virtualNetwork1"
-  location            = azurerm_resource_group.rg-atomic-red-team.location
-  resource_group_name = azurerm_resource_group.rg-atomic-red-team.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  depends_on                   = [azurerm_network_security_group.atomic-nsg]
+  name                         = "virtualNetwork1"
+  location                     = azurerm_resource_group.rg-atomic-red-team.location
+  resource_group_name          = azurerm_resource_group.rg-atomic-red-team.name
+  address_space                = ["10.0.0.0/16"]
+  dns_servers                  = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
-    name              = "atomic-snet"
-    address_prefix    = "10.0.3.0/24"
-    security_group    = azurerm_network_security_group.atomic-nsg.id
+    name                       = "atomic-snet"
+    address_prefix                      = "10.0.3.0/24"
+    security_group             = azurerm_network_security_group.atomic-nsg.id
   }
 
   tags = {
-    modules           = "atomic-red-team"
-    author            = "Sacha Roussakis-Notter (DFW1N)"
-    repo              = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
-    assessment        = "red-teaming-activity"
+    modules                    = "atomic-red-team"
+    author                     = "Sacha Roussakis-Notter (DFW1N)"
+    repo                       = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment                 = "red-teaming-activity"
   }
 }
 
@@ -67,9 +68,9 @@ resource "azurerm_virtual_network" "atomic-vnet" {
 ############################
 
 resource "azurerm_network_security_group" "atomic-nsg" {
-  name                = "AtomicRedTeamNetworkSecurityGroup"
-  location            = azurerm_resource_group.rg-atomic-red-team.location
-  resource_group_name = azurerm_resource_group.rg-atomic-red-team.name
+  name                         = "AtomicRedTeamNetworkSecurityGroup"
+  location                     = azurerm_resource_group.rg-atomic-red-team.location
+  resource_group_name          = azurerm_resource_group.rg-atomic-red-team.name
   
   security_rule {
     name                       = "AtomicAllowAll"
@@ -84,9 +85,65 @@ resource "azurerm_network_security_group" "atomic-nsg" {
   }
 
   tags = {
-    modules           = "Atomic-Red-Team"
-    author            = "Sacha Roussakis-Notter (DFW1N)"
-    repo              = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
-    assessment        = "red-teaming-activity"
+    modules                    = "Atomic-Red-Team"
+    author                     = "Sacha Roussakis-Notter (DFW1N)"
+    repo                       = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment                 = "red-teaming-activity"
+  }
+}
+
+#############################
+## Azure Public IP Address ##
+#############################
+
+resource "azurerm_public_ip" "atomic-pip" {
+  name                         = "atomic-public-ip"
+  resource_group_name          = azurerm_resource_group.rg-atomic-red-team.name
+  location                     = azurerm_resource_group.rg-atomic-red-team.location
+  allocation_method            = "Dynamic"
+
+  tags = {
+    modules                    = "atomic-red-team"
+    author                     = "Sacha Roussakis-Notter (DFW1N)"
+    repo                       = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment                 = "red-teaming-activity"
+  }
+
+}
+
+###################
+## Random String ##
+###################
+
+resource "random_string" "random-atomic-strings" {
+  length                       = 8
+  special                      = false
+  lower                        = true
+  upper                        = false
+  number                       = true
+}
+
+#######################################
+## Virtual Machine Network Interface ##
+#######################################
+
+resource "azurerm_network_interface" "atomic-nic-pip" {
+  depends_on                   = [azurerm_public_ip.atomic-pip]
+  name                         = "atomic-win-${random_string.random-atomic-strings.result}-vm-nic"
+  resource_group_name          = azurerm_resource_group.rg-atomic-red-team.name
+  location                     = azurerm_resource_group.rg-atomic-red-team.location
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  tags = {
+    modules                    = "atomic-red-team"
+    author                     = "Sacha Roussakis-Notter (DFW1N)"
+    repo                       = "https://github.com/DFW1N/terraform-azure-atomic-red-team"
+    assessment                 = "red-teaming-activity"
   }
 }
